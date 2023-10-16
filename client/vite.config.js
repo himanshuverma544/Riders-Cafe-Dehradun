13a55a7d618b63995874cbaef18d0d04282db2b4
Himanshu Verma <himanshuverma544@gmail.com>
2023-10-16 18:43:12 +0530

    upd: added the configurations for eslint and svgr

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+import svgr from 'vite-plugin-svgr';
+import eslint from 'vite-plugin-eslint';
+
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    svgr({
+      svgrOptions: {
+        ref: true,
+      },
+    }),
+
+    eslint({
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: [],
+    })
+  ],
+});
