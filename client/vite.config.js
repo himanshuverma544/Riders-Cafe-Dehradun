e33c024a02e33beb4c17551c4edcef12de2980a9
Himanshu Verma <himanshuverma544@gmail.com>
2023-10-17 15:40:27 +0530

    indent: indentation fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 import svgr from 'vite-plugin-svgr';
 import eslint from 'vite-plugin-eslint';
 
 
 export default defineConfig({
+  
   plugins: [
     react(),
 
     svgr({
       svgrOptions: {
-        ref: true,
-      },
+        ref: true
+      }
     }),
 
     eslint({
       cache: false,
       include: ['./src/**/*.js', './src/**/*.jsx'],
-      exclude: [],
+      exclude: []
     })
-  ],
+  ]
 });
