a93c90aaabec970e7df25620321609db5c9d8fb0
Himanshu Verma <himanshuverma544@gmail.com>
2023-11-07 12:16:34 +0530

    add: added constants and its file

---
+++ client/src/constants.js
@@ -0,0 +1,18 @@
+export const HOME = {
+  title: "Home",
+  path: "/"
+};
+
+export const MENU = {
+  title: "Menu",
+  path: "/menu"
+};
+
+export const YOUR_ORDERS = {
+  title: "Your Orders",
+  path: "/your-orders"
+};
+
+export const PAGE_404 = {
+  path: "*"
+};
