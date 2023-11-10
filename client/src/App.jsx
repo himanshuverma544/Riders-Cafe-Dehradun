ac1d46f2678dd14b94fd5da3ff9f1ad26fac081f
Himanshu Verma <himanshuverma544@gmail.com>
2023-11-10 12:54:40 +0530

    upd: added up the routes to the required pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css';
 
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import YourOrders from "./pages/YourOrders";
+import Authentication from "./pages/Authentication";
+import Page404 from "./pages/Page404";
+
+import { HOME, MENU, YOUR_ORDERS, AUTHENTICATION, PAGE_404 } from "./constants";
+
+
 function App() {
 
   return (
-    <>
-      Hello World
-    </>
-  )
+    <HashRouter basename=''>
+      <Routes>
+        <Route path={HOME.path} element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path={MENU.path} element={<Menu/>}/>
+          <Route path={YOUR_ORDERS.path} element={<YourOrders/>}/>
+          <Route path={AUTHENTICATION.path} element={<Authentication/>}/>
+          <Route path={PAGE_404.path} element={<Page404/>}/>
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
