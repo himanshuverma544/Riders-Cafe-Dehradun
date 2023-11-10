401fe56c78ed1daee35507cab5095a88db7e22e9
Himanshu Verma <himanshuverma544@gmail.com>
2023-11-10 12:55:29 +0530

    upd: wrapped the app component with theme provider and cssbaseline

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { theme } from "../mui.theme.js";
+
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <App/>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </>
+);
