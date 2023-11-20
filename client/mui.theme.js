171db5c490eee0ff972aec45c8a15740ee573f7b
Himanshu Verma <himanshuverma544@gmail.com>
2023-11-20 11:56:57 +0530

    upd: corrected the mui theme as per tailwind css

--- client/mui.theme.js
+++ client/mui.theme.js
@@ -2,12 +2,16 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
   breakpoints: {
+
+    /* Subtracting '1', as MUI 639 is equal to Tailwind 640, 
+    follows the same for the rest of the breakpoints too */
+
     values: {
-      sm: 640,
-      md: 768,
-      lg: 1024,
-      xl: 1280,
-      ["2xl"]: 1536
+      sm: 640 - 1, 
+      md: 768 - 1,
+      lg: 1024 - 1,
+      xl: 1280 - 1,
+      xxl: 1536 - 1
     },
   },
 });
