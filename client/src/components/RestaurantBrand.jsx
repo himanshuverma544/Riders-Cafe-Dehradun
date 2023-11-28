a57ef008d693216c8ec4bc8621cd11481022dd03
Himanshu Verma <himanshuverma544@gmail.com>
2023-11-28 11:09:46 +0530

    add: brand name and logo in separate component

---
+++ client/src/components/RestaurantBrand.jsx
@@ -0,0 +1,29 @@
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@mui/material/Link";
+
+
+const RestaurantBrand = ({ name, logo, variant }) => {
+
+  return (
+    <>
+      <Link 
+        className="restaurant-brand w-fit flex items-center gap-3" 
+        component={RouterLink}
+        variant={variant} 
+        to="/" 
+        fontSize="2rem"
+        underline="none"
+        color="#FFF"
+      >
+        <img
+          className="max-w-[2.1rem] h-auto"
+          src={logo}
+          alt="Restaurant-Brand-Logo-Image"
+        />
+        {name}
+      </Link>
+    </>
+  );
+}
+
+export default RestaurantBrand;
